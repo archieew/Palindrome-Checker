465b58fb3c8e9be6ae2dc448cf3170ad3f7b199b
Archie Custodio <archie06custodio@gmail.com>
2025-07-15 20:33:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-script.js
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+checkButton.addEventListener("click",() => {
+  if (textInput.value === "") {
+    alert("Pleast input a value")
+  }
+});
