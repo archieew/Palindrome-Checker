a3f686ce299e8c916ec49bd97d81d2227755e941
Archie Custodio <archie06custodio@gmail.com>
2025-07-15 21:41:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
 checkButton.addEventListener("click",() => {
+  const replaced = textInput.value.replace(/[^A-Za-z]/)
+
   if (textInput.value === "") {
-    alert("Pleast input a value")
+    alert("Please input a value")
+  } else if (textInput.value.length === 1) {
+    result.innerText = `${textInput.value} is a palindrome`
+  } else if (textInput.value === [...textInput.value].reverse().join("")) {
+    result.innerText = `${textInput.value} is a palindrome`
   }
 });
